7cadc92120825afc57c12a3a2773f1cada40e250
Josue Palomo <josuepalomocs@gmail.com>
2023-03-06 13:15:05 -0600

    init commit

---
+++ src/main/java/com/palomo/josue/ocfades_server/OcfadesServerApplication.java
@@ -0,0 +1,13 @@
+package com.palomo.josue.ocfades_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcfadesServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OcfadesServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/palomo/josue/ocfades_server/OcfadesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palomo.josue.ocfades_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcfadesServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
